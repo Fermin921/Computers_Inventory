0638ded161b05b19a7d37cd5fb50597b65f614a1
Fermin921 <ferminmartinez921@gmail.com>
2024-05-29 15:39:42 -0500

    1.0 Se agrego diseño: -Vista add computer -Pantalla login -Pantalla Details computer(falta)

--- app/src/main/java/com/example/computers_inventory/Add_Computer.kt
+++ app/src/main/java/com/example/computers_inventory/Add_Computer.kt
@@ -7,13 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Add_Computer : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.add_computer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.add_computer)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/computers_inventory/Details_Computer.kt
+++ app/src/main/java/com/example/computers_inventory/Details_Computer.kt
@@ -7,13 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Details_Computer: AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.details_computer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.divCard)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
